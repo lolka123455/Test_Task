41e545b6d1f2bc7c5d61b01f436f929fbe9529c3
Alexander Tyryatkin <altyryatkin@gmail.com>
2022-11-21 18:49:36 +0300

    Add navigation in app module

---
+++ app/src/main/java/com/example/testtask/effective/App.kt
@@ -0,0 +1,28 @@
+package com.example.testtask.effective
+
+import android.app.Application
+import com.example.testtask.cart_screen.di.CartScreenModule
+import com.example.testtask.detail_screen.di.DetailScreenModule
+import com.example.testtask.main_screen.di.MainScreenModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidLogger(Level.DEBUG)
+            androidContext(this@App)
+            modules(
+                CartScreenModule,
+                MainScreenModule,
+                DetailScreenModule
+            )
+        }
+    }
+}
--- app/src/main/java/com/example/testtask/effective/MainActivity.kt
+++ app/src/main/java/com/example/testtask/effective/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.testtask.effective
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import com.example.testtask.effective.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var navHostFragment: NavHostFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        navHostFragment.findNavController().handleDeepLink(intent)
     }
 }
