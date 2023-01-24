327c25d63e84044ffcf3af9acf1896bb91dc3152
Alexander Tyryatkin <altyryatkin@gmail.com>
2023-01-24 17:39:03 +0300

    Add documentation

--- state_network_connection/src/main/java/com/example/testtask/state_network_connection/FetchResult.kt
+++ state_network_connection/src/main/java/com/example/testtask/state_network_connection/FetchResult.kt
@@ -1,7 +1,36 @@
 package com.example.testtask.state_network_connection
 
+/**
+ * This is a sealed class that represents the different types of results that can occur when fetching data from a data source.
+ *
+ * @property SuccessDataUpload a subclass that represents a successful data upload
+ * @property ErrorLoadingData a subclass that represents an error while loading data
+ *
+ * @constructor Creates a new instance of the FetchResult
+ *
+ */
+
 sealed class FetchResult<T> {
 
+    /**
+     * A subclass that represents a successful data upload.
+     *
+     * @property data the data that was successfully uploaded
+     *
+     * @constructor Creates a new instance of the SuccessDataUpload
+     *
+     */
+
     class SuccessDataUpload<T>(val data: T) : FetchResult<T>()
+
+    /**
+     * A subclass that represents an error while loading data.
+     *
+     * @property message an optional message describing the error
+     *
+     * @constructor Creates a new instance of the ErrorLoadingData
+     *
+     */
+
     class ErrorLoadingData<T>(val message: String?) : FetchResult<T>()
 }   
--- state_network_connection/src/main/java/com/example/testtask/state_network_connection/UiState.kt
+++ state_network_connection/src/main/java/com/example/testtask/state_network_connection/UiState.kt
@@ -1,5 +1,14 @@
 package com.example.testtask.state_network_connection
 
+/**
+ * This is a sealed class that represents the different states of the UI.
+ *
+ * @property Success represents a successful state
+ * @property Error represents an error state
+ * @property Loading represents a loading state
+ *
+ */
+
 sealed class UiState {
 
     object Success : UiState()
