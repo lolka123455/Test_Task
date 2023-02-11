a1b46f0065a938bd82cfd1107c1b55c047c171e3
Alexander Tyryatkin <altyryatkin@gmail.com>
2023-02-11 15:40:31 +0300

    Fixed wrong implementation of fast adapter, code refactoring

--- cart_screen/src/main/java/com/example/testtask/cart_screen/fragments/CartScreenFragment.kt
+++ cart_screen/src/main/java/com/example/testtask/cart_screen/fragments/CartScreenFragment.kt
@@ -8,34 +8,40 @@ import android.widget.FrameLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.testtask.cart_screen.adapters.CartAdapter
+import com.example.testtask.cart_screen.adapters.CartItemAdapter
 import com.example.testtask.cart_screen.databinding.FragmentCartBinding
-import com.example.testtask.cart_screen.adapters.CartItemsAdapter
 import com.example.testtask.cart_screen.viewmodel.CartViewModel
 import com.example.testtask.state_network_connection.R
 import com.example.testtask.state_network_connection.UiState
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
+import com.mikepenz.fastadapter.FastAdapter
+import com.mikepenz.fastadapter.adapters.ItemAdapter
+import com.mikepenz.fastadapter.diff.FastAdapterDiffUtil
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class CartScreenFragment : Fragment() {
 
-    private lateinit var binding: FragmentCartBinding
+    private val binding: FragmentCartBinding
+        get() = _binding!!
+    private var _binding: FragmentCartBinding? = null
     private val viewModel: CartViewModel by viewModel()
-    private val adapter: CartAdapter = CartAdapter()
+
+    private val itemAdapter: ItemAdapter<CartItemAdapter> = ItemAdapter()
+    private val recyclerViewAdapter = FastAdapter.with(itemAdapter)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentCartBinding.inflate(inflater, container, false)
+        _binding = FragmentCartBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.recyclerView.adapter = adapter
-        binding.backImageView.setOnClickListener { requireActivity().onBackPressed() }
+        binding.recyclerView.adapter = recyclerViewAdapter
+        binding.backImageView.setOnClickListener {
+            requireActivity().supportFragmentManager.popBackStack()
+        }
 
         observe()
         setupRecyclerView()
@@ -48,28 +54,32 @@ class CartScreenFragment : Fragment() {
         observeDelivery()
     }
 
-    private fun observeUIState(){
-        viewModel.uiState
-            .onEach { updateUiState(it) }
-            .launchIn(viewLifecycleOwner.lifecycleScope)
+    private fun observeUIState() {
+        lifecycleScope.launchWhenStarted {
+            viewModel.uiState.collect { updateUiState(it) }
+        }
     }
 
-    private fun observeCartItems(){
-        viewModel.cartItems
-            .onEach { adapter.submitList(it) }
-            .launchIn(viewLifecycleOwner.lifecycleScope)
+    private fun observeCartItems() {
+        lifecycleScope.launchWhenStarted {
+            viewModel.cartItems.collect {
+                FastAdapterDiffUtil[itemAdapter] = it.map { cartItem ->
+                    CartItemAdapter(cartItem)
+                }
+            }
+        }
     }
 
-    private fun observeTotal(){
-        viewModel.total
-            .onEach { binding.totalTextView.text = it.toPriceFormat() }
-            .launchIn(viewLifecycleOwner.lifecycleScope)
+    private fun observeTotal() {
+        lifecycleScope.launchWhenStarted {
+            viewModel.total.collect { binding.totalTextView.text = it.toPriceFormat() }
+        }
     }
 
-    private fun observeDelivery(){
-        viewModel.delivery
-            .onEach { binding.deliveryTextView.text = it }
-            .launchIn(viewLifecycleOwner.lifecycleScope)
+    private fun observeDelivery() {
+        lifecycleScope.launchWhenStarted {
+            viewModel.delivery.collect { binding.deliveryTextView.text = it }
+        }
     }
 
     private fun updateUiState(state: UiState) {
@@ -102,7 +112,7 @@ class CartScreenFragment : Fragment() {
 
     private fun setupRecyclerView() {
         with(binding.recyclerView) {
-            adapter = this@CartScreenFragment.adapter
+            adapter = this@CartScreenFragment.recyclerViewAdapter
             layoutManager = LinearLayoutManager(requireContext())
         }
     }
@@ -119,4 +129,9 @@ class CartScreenFragment : Fragment() {
         if (this >= 1000) {
             "$%.${3}f us".format(this / 1000.0)
         } else "$$this us"
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
